3325bfe5b1566140efb6857c40d5ad792b9d5377
andrewp113 <asp113@georgetown.edu>
2022-10-26 18:15:47 -0400

    Display latest data

--- app/unemployment.py
+++ app/unemployment.py
@@ -14,5 +14,15 @@ request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={
 response = requests.get(request_url)
 
 parsed_response = json.loads(response.text)
-print(type(parsed_response))
-pprint(parsed_response)
+#print(type(parsed_response))
+#pprint(parsed_response)
+
+# Challenge A
+#
+# What is the most recent unemployment rate? And the corresponding date?
+# Display the unemployment rate using a percent sign.
+
+#breakpoint()
+
+latest = parsed_response["data"][0]
+print(latest)
