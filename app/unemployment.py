23f409be272078b6535f21ab870f98d9a0fa5181
andrewp113 <asp113@georgetown.edu>
2022-10-27 18:03:40 -0400

    Challenge B - average unemployment rate

--- app/unemployment.py
+++ app/unemployment.py
@@ -4,6 +4,7 @@
 import os
 import json
 from pprint import pprint
+from statistics import mean
 
 import requests
 
@@ -24,5 +25,24 @@ parsed_response = json.loads(response.text)
 
 #breakpoint()
 
-latest = parsed_response["data"][0]
-print(latest)
+latest = parsed_response["data"]
+
+print("-------------------------")
+print("LATEST UNEMPLOYMENT RATE:")
+print(f"{latest[0]['value']}%", "as of", latest[0]["date"])
+#print(latest)
+
+
+# Challenge B
+# 
+# What is the average unemployment rate for all months during this calendar year?
+# ... How many months does this cover?
+
+this_year = [d for d in latest if "2022-" in d["date"]]
+
+rates_this_year = [float(d["value"]) for d in this_year]
+#print(rates_this_year)
+
+print("-------------------------")
+print("AVG UNEMPLOYMENT THIS YEAR:", f"{mean(rates_this_year)}%")
+print("NO MONTHS:", len(this_year))
